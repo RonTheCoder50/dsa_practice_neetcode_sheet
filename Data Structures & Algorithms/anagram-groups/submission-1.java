580738369d0a7af0fe64969dd7dbbdbca14b7e3c
Rohan Vaybhase <rohanvaybhase50@gmail.com>
2026-07-02 14:07:18 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Optimal solution avoid sorting & array (char) for each string.
+        // TC will be from O(n * (n*m)) -> ?
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+            //everytime count frequency
+            int freq[] = new int[26];
+            for(char ch: str.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+
+            //build unique key via stringbuilder
+            StringBuilder sb = new StringBuilder();
+            for(int count : freq) {
+                sb.append("#").append(count);
+            }
+
+            String key = sb.toString(); //it will generate unique key.
+            
+            //put if key is newer/unique & map those corrosponding strings with it.
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
